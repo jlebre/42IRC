f2df14e4f46a47c88826c9dd2c71c64adcf4fe81
jlebre <josemariamlebre@gmail.com>
2024-06-14 14:44:50 +0100

    update

--- ft_irc/main.cpp
+++ ft_irc/main.cpp
@@ -2,7 +2,8 @@
 
 bool run = true;
 
-void handleSignal(int signo) {
+void handleSignal(int signo)
+{
 	(void)signo;
 	run = false;
 }
@@ -12,6 +13,18 @@ int main(int argc, char **argv)
 	if (argc != 3)
 	{
 		std::cerr << "Usage: " << argv[0] << " <port> <password>" << std::endl;
+		return 1;
+	}
+	std::string port = argv[1];
+	if (port.find_first_not_of("0123456789") != std::string::npos)
+	{
+		std::cout << "Port should be a number" << std::endl;
+		return 1;
+	}
+	int port_i = atoi(port.c_str());
+    if (port_i < 1024 || port_i > 65534)
+	{
+		std::cerr << "Port should be a number between 1024 and 65534\n";
 		return 1;
 	}
 		
